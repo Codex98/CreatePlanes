4b771108948abe36ccaa7aa60330513408af7178
Codex98 <47092106+Codex98@users.noreply.github.com>
2019-01-27 18:04:31 -0600

    Add files via upload

---
+++ P38.py
@@ -0,0 +1,47 @@
+#Could we perhaps make a feild guide in which you enter these perameters and the guide identifies the plane?
+print("Create YOUR OWN Military planes!")
+name = input("What is YOUR plane called?")
+color = input("What color and/or paint job is your plane?")
+weapons = input("How many weapons are on your plane?")
+sides = input("Who's side are you on? (Allies or Axis?)")              
+endurance = input("How long can your plane fly on a single tank of fuel?")
+weaknesses = input("What are your planes weak spots?")
+engines = input("How many engines does your plane have?")
+crew = input("What is your plane's crew? (Including you.)")
+bombs = input("can your plane carry bombs? if so, how many?")
+landing = input("Can your plane land on an aircraft carrier?")
+you = input("How long have you been flying?")
+# may we do a if or elif here? if you've been flying for more than 4 years your a seasoned pilot? if less, your a rookie?
+print("Your plane's name is", name)
+print("Your plane is", color)
+print("Your plane has", weapons)
+print("Your side is the", sides)
+print("Your plane's endurance is", endurance)
+print("Your plane's weaknesses are", weaknesses,"...be careful, pilot!")
+print("Number of engines:", engines)
+print("Crew:", crew)
+print("Your plane's bomb capacity is", bombs)
+print("Your plane's aircraft carrier tolerance:", landing)
+print("You've been flying for", you)
+print("You're a Squadron Leader")
+print("Good luck,", color, "leader!")
+print("Remember, You've still got to make your other planes; you've got a squadron of 5 planes of your choice! Fill out the questions below to customize them!")
+name2 = input("What's your squadron called?")
+color2 = input("What's your squadron's principle color scheme?")
+weapons2 = input ("What are the weapon's on the planes in your squadron?")
+landing2 = input ("Can your squad land on an aircraft carrier?")
+print("You're squadron is called:", name2)
+print("You're squadron's color is:", color2)
+print("You're squadron has the following weapons:", weapons2)
+print("Can you're sqaudron land on an aircraft carrier?", landing2)
+print("You can now get in the air and fight! Good luck,", name2,"!")
+print("END TRANSMISSION")
+#if i was to make a game could i use this information to generate an image of a plane? say, if you filled out
+#how many engines does your plane have?: 4
+# then the computer should generate an image in the database. say i put an image of a fighter sillhouette, plus a 2 engine and a four engine, and then i finally type in the code.
+#it should assign a plane to each answer. if i assign it in a 'fixed world'.
+#STUDY IF AND ELIF!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+    
+            
+                
